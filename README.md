83885ee1173d80905d06172602ec10f900e0f3d2
rviollet <contact.romain.git@gmail.com>
2022-05-13 14:39:03 +0200

    landing page demo with tailwind

---
+++ README.md
@@ -0,0 +1,3 @@
+# Landing Page Demo created with the framework TailWind CSS
+
+![alt text](https://github.com/romainviollet/landing-page-tailwind/blob/main/src/pictures/print-screen-landing-page.png?raw=true)
---
+++ app.js
@@ -0,0 +1,8 @@
+//Toggle Menu
+const menuElementsToToggle = document.querySelectorAll(".toggle-menu");
+const iconeToggle = document.querySelector(".icone-toggle");
+
+const toggleMenu = () =>
+  menuElementsToToggle.forEach((el) => el.classList.toggle("hidden"));
+
+iconeToggle.addEventListener("click", toggleMenu);
